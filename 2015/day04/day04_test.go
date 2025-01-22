d4ad808e5a5f6c57ea11262e0cc1de6f934d62d3
Sunil Jose <bsuniljose25@gmail.com>
2025-01-22 22:30:56 +0530

    Added benchmarks for day04

--- 2015/day04/day04_test.go
+++ 2015/day04/day04_test.go
@@ -2,6 +2,7 @@ package day04
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -16,5 +17,19 @@ func TestGetHash(t *testing.T) {
 	if r := getHash("abcdef609043"); r != "000001dbbfa3a5c83a2d506429c7b00e" {
 		fmt.Printf("expected: 000001dbbfa3a5c83a2d506429c7b00e, obtained: %v\n", r)
 		t.Fail()
+	}
+}
+
+func BenchmarkGetHashes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		getHash("abcdef123456")
+	}
+}
+
+func Benchmark1_000_000GetHashes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for i := 0; i < 1_000_000; i++ {
+			getHash("abcdef123456" + strconv.Itoa(i))
+		}
 	}
 }
